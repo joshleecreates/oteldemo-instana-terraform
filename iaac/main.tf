e04e44cd15032fb2b1217b67e05075ceeec3ff9f
Joshua Lee <Joshua.Michael.Lee@ibm.com>
2024-01-09 13:00:26 -0500

    Initial commit

---
+++ iaac/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    kind = {
+      source  = "tehcyx/kind"
+      version = "0.2.1"
+    }
+  }
+}
+
+provider "kind" {}
+
+resource "kind_cluster" "default" {
+  name = "default-cluster"
+  kind_config {
+    kind        = "Cluster"
+    api_version = "kind.x-k8s.io/v1alpha4"
+    node {
+      role = "control-plane"
+
+      kubeadm_config_patches = [
+        "kind: InitConfiguration\nnodeRegistration:\n  kubeletExtraArgs:\n    node-labels: \"ingress-ready=true\"\n"
+      ]
+
+      extra_port_mappings {
+        container_port = 8080
+        host_port      = 8080
+      }
+    }
+  }
+}
